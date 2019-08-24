468895559b519d34fc5f30c7d69ef34eb5b552f9
bkmzli1 <43925220+bkmzli1@users.noreply.github.com>
2019-08-24 21:05:41 +0500

    up fix

--- src/main/java/ru/bkmz/installer/controller/Update.java
+++ src/main/java/ru/bkmz/installer/controller/Update.java
@@ -1,4 +1,33 @@
 package ru.bkmz.installer.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import ru.bkmz.installer.util.PaneNext;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Update {
+    @FXML
+    public Button urlB;
+    public Button next;
+    public AnchorPane rootPane;
+
+    public void onUelB(ActionEvent actionEvent) {
+        try {
+            Desktop.getDesktop().browse(new URL("https://github.com/bkmzli1/InstallerDrizzle/blob/master/build/launch4j/Installer%20drizzle.exe").toURI());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void next(ActionEvent actionEvent) {
+        new PaneNext(rootPane, "fxml/installer/main.fxml");
+    }
 }
